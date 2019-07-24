256b5000e6d4c966209f14f75bf1648b1609fd2d
Maggie Johnson <maggie@Maggies-MacBook-Pro.local>
2019-07-24 13:56:12 -0500

    added clear input field instruction

--- scripts/faves.js
+++ scripts/faves.js
@@ -5,11 +5,18 @@ document.querySelector("#saveEntry").addEventListener("click", event => {
     const location = document.querySelector("#location").value
     const message = `I can purchase ${wishlistItem} at ${location}`
 
+    const clearItem = document.querySelector("#wishlistItem").value = ""
+    const clearLocation = document.querySelector("#location").value = ""
+
     console.log(message)
 
     container.innerHTML += `<section>I can purchase ${wishlistItem} at ${location} </section>`
 
+
 }
 )
+
+
+
 
 
